a3f76f8ba3af686801834f57f1fe8386706f9889
Arihant Pirale <arihantpirale@Arihants-MacBook-Air.local>
2024-10-13 18:55:10 +0530

    dirname added

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "10mb" })) // allows you to parse the request body
 app.use(cookieParser())
 
